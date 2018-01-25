5621ced9162a436a74b9ea676da6ee6863da7f24
tara z. manicsic <tzmanics@gmail.com>
2018-01-25 10:54:12 -0500

    change up and & grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 import { DropDownList } from '@progress/kendo-react-dropdowns';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+import { orderBy } from '@progress/kendo-data-query';
+import '@progress/kendo-theme-default/dist/all.css';
 import './App.css';
-import '@progress/kendo-theme-material';
+
+import nutrition from './nutrition.json';
 
 class App extends Component {
-  ratings = ['Rad', 'Awesome', 'Stellar', 'Interstellar 👽'];
+  constructor(props) {
+    super(props);
+    this.state = {
+      ratings: ['Rad', 'Awesome', 'Stellar', 'Interstellar 👽'],
+      nutrition: this.getNutrition([]),
+      sort: [],
+      allowUnsort: true
+    };
+    this.sortChange = this.sortChange.bind(this);
+  }
+
+  sortChange = (event) => {
+    this.setState({
+      nutrition: this.getNutrition(event.sort),
+      sort: event.sort
+    });
+  }
+
+  getNutrition = (sort) => orderBy(nutrition, sort);
 
   render() {
     return (
       <div className="App">
-        <DropDownList data={this.ratings} />
+        <Grid
+          style={{ maxHeight: '500px'}}
+          data={this.state.nutrition}
+          sortable={{
+            allowUnsort: this.state.allowUnsort,
+            mode: 'multiple'
+          }}
+          sort={this.state.sort}
+          sortChange={this.sortChange}>
+          <Column field='Description' title='Food' />
+          <Column field='Measure' title='Amount' />
+          <Column field='Protein(g)Per Measure' title='Protein' />
+          <Column field='Carbohydrate, by difference(g)Per Measure' title='Carbs' />
+          <Column field='Sugars, total(g)Per Measure' title='Sugars' />
+        </Grid>
+        <DropDownList data={this.state.ratings} />
       </div>
     );
   }
